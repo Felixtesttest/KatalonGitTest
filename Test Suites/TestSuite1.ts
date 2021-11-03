5fb10b152f091cf153f3fe7d64bd5a89975a50bf
Felixtesttest <hkbutest01@gmail.com>
2021-11-03 17:13:23 +0800

    test message : Tutotial 16 , Jenkins + Git

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9060f2b6-8226-41d5-8784-f93863abda78</testSuiteGuid>
+   <testCaseLink>
+      <guid>52a8ac1f-b798-4857-8374-d7e460e6c334</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
